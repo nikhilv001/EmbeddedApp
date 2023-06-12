3213ef07c34a10113faef0f12421eb3d859b00b8
Nikhil Verma <nikhilverma@nikhil.verma-C02D14VYMD6N>
2023-06-12 18:53:01 +0530

    themechanged Event listener

--- index.js
+++ index.js
@@ -5,9 +5,31 @@ const app = new window.Webex.Application();
 // Wait for onReady() promise to fulfill before using framework
 app.onReady().then(() => {
     log("App ready. Instance", app);
+
+    // Listen and emit any events from the EmbeddedAppSDK
+    app.listen().then(() => {
+        app.on("application:themeChanged", (payload) =>
+          log("application:themeChanged", payload)
+        );
+        // app.on("application:displayContextChanged", (payload) =>
+        //   log("application:displayContextChanged", payload)
+        // );
+        // app.on("application:shareStateChanged", (payload) =>
+        //   log("application:shareStateChanged", payload)
+        // );
+        // app.on("meeting:infoChanged", (payload) =>
+        //   log("meeting:infoChanged", payload)
+        // );
+        // app.on("meeting:roleChanged", (payload) =>
+        //   log("meeting:roleChanged", payload)
+        // );
+        // app.on("space:infoChanged", (payload) => log("space:infoChanged", payload));
+      });
+
 }).catch((errorcode) =>  {
     log("Error with code: ")
 });
+
 
 // Button click handler to set share URL
 function handleSetShare() {
